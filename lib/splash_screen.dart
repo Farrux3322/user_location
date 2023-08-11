fd25afd6bfb7dcd5f39c5fc22185684906ae8f8d
Farrux Madaminov +998937373322 <madaminovfarrux3322@gmail.com>
2023-08-11 16:13:34 +0500

    Initial commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:lottie/lottie.dart';
+import 'package:user_location/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+  LatLng? latLng;
+
+  Future<void> _getLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    locationData = await location.getLocation();
+
+    setState(() {
+      latLng = LatLng(locationData.latitude!, locationData.longitude!);
+    });
+  }
+
+  _init() async {
+    await _getLocation();
+
+    await Future.delayed(const Duration(seconds: 1));
+
+    if (context.mounted&&latLng!=null) {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return MapSample(
+          latLng: latLng!,
+        );
+      }));
+    }
+  }
+
+  @override
+  void initState() {
+    _init();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset("assets/images/splash_logo.json"),
+      ),
+    );
+  }
+}
